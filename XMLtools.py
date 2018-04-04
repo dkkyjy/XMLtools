91a6619b21b3c28d474fce91e320e1360c743f8c
dkkyjy <duankk@pmo.ac.cn>
2018-04-04 14:49:44 +0800

    update

--- XMLtools.py
+++ XMLtools.py
@@ -10,25 +10,71 @@ def prettify(elem):
     return reparsed.toprettyxml(indent="")
 
 
-class NewModel(object):
-    def SaveModel(self, filename):
-        tree = ET.ElementTree(self.root)
-        tree.write(filename)
+class Model(object):
+    def GetModelInfo(self, root):
+        self.SrcList = []
+        self.FixSrcList = []
+        self.FreeSrcList = []
 
+        self.ParList = []
+        self.FixParList = []
+        self.FreeParList = []
+
+        self.ParDict = {}
+        self.FixParDict = {}
+        self.FreeParDict = {}
+
+        for source in root:
+            srcName = source.get('name')
+            self.SrcList.append(srcName)
+            ParList = []
+            FixParList = []
+            FreeParList = []
+            for parameter in source.findall('.//spectrum/parameter'):
+                parName = parameter.get('name')
+                ParList.append(parName)
+                self.ParList.append(srcName + '__' + parName)
+                if parameter.get('free') == '0':
+                    FixParList.append(parName)
+                    self.FixParList.append(srcName + '__' + parName)
+                if parameter.get('free') == '1':
+                    FreeParList.append(parName)
+                    self.FreeParList.append(srcName + '__' + parName)
+            self.ParDict[srcName] = ParList
+            self.FixParDict[srcName] = FixParList
+            self.FreeParDict[srcName] = FreeParList
+            if FreeParList:
+                self.FreeSrcList.append(srcName)
+            else:
+                self.FixSrcList.append(srcName)
+
+        self.SrcNum = len(self.SrcList)
+        self.FixSrcNum = len(self.FixSrcList)
+        self.FreeSrcNum = len(self.FreeSrcList)
+
+        self.ParNum = len(self.ParList)
+        self.FixParNum = len(self.FixParList)
+        self.FreeParNum = len(self.FreeParList)
+
+
+class NewModel(Model):
     def __init__(self):
         self.root = ET.Element('source_library', title='source_library')
 
     def AddSrcEle(self, srcName, srcEle):
         source = self.root.find('./source[@name="%s"]' % srcName)
         source.extend(srcEle)
+        self.GetModelInfo(self.root)
 
     def AddSpectialEle(self, srcName, spectialEle):
         spectrum = self.root.find('./source[@name="%s"]/spectrum' % srcName)
         spectrum.extend(spectialEle)
+        self.GetModelInfo(self.root)
 
     def AddSpatialEle(self, srcName, spatialEle):
         spatialModel = self.root.find('./source[@name="%s"]/spatialModel' % srcName)
         spatialModel.extend(spatialEle)
+        self.GetModelInfo(self.root)
 
     def AddPointSource(self, srcName, SpectralType=None, SpectralPars=None, skycrd_C=None):
         source = ET.Element('source', name=srcName, type='PointSource')
@@ -57,6 +103,7 @@ class NewModel(object):
         except:
             pass
         self.root.append(source)
+        self.GetModelInfo(self.root)
 
     def AddDiffuseSource(self, srcName, SpectralType=None, SpectralPars=None, SpatialType=None, SpatialFile=None, SpatialPar=None):
         source = ET.Element('source', name=srcName, type='DiffuseSource')
@@ -88,6 +135,11 @@ class NewModel(object):
         except:
             pass
         self.root.append(source)
+        self.GetModelInfo(self.root)
+
+    def SaveModel(self, filename):
+        tree = ET.ElementTree(self.root)
+        tree.write(filename)
 
 
 class LoadModel(NewModel):
@@ -96,36 +148,7 @@ class LoadModel(NewModel):
         self.filename = filename
         self.tree = ET.parse(filename)
         self.root = self.tree.getroot()
-
-        self.SrcList = []
-        self.FixSrcList = []
-        self.FreeSrcList = []
-
-        self.ParList = []
-        self.FixParList = []
-        self.FreeParList = []
-
-        for source in self.root:
-            srcName = source.get('name')
-            self.FixSrcList.append(srcName)
-            for parameter in source.findall('.//spectrum/parameter'):
-                self.SrcList.append(srcName)
-                parName = parameter.get('name')
-                self.ParList.append(srcName + '__' + parName)
-                if parameter.get('free') == '0':
-                    self.FixParList.append(srcName + '__' + parName)
-                if parameter.get('free') == '1':
-                    self.FreeParList.append(srcName + '__' + parName)
-                    self.FreeSrcList.append(srcName)
-                    try:
-                        self.FixSrcList.remove(srcName)
-                    except:
-                        pass
-
-        self.ParsNumDict = dict(Counter(self.SrcList))
-        self.SrcList = list(self.ParsNumDict.keys())
-        self.FreeNumDict = dict(Counter(self.FreeSrcList))
-        self.FreeSrcList = list(self.FreeNumDict.keys())
+        self.GetModelInfo(self.root)
 
     def GetSrcInfo(self, srcName):
         source = self.root.find('./source[@name="%s"]' % srcName)
@@ -148,7 +171,7 @@ class LoadModel(NewModel):
             dec = float(source.attrib['DEC'])
         return (ra, dec)
         
-    def GetSpectralType(self, srcName):
+    def GetSpectralInfo(self, srcName):
         spectrum = self.root.find('./source[@name="%s"]/spectrum' % srcName)
         pprint(spectrum.tag)
         pprint(spectrum.attrib)
@@ -157,7 +180,11 @@ class LoadModel(NewModel):
         spectrum = self.root.find('./source[@name="%s"]/spectrum' % srcName)
         return spectrum
 
-    def GetSpatialType(self, srcName):
+    def GetSpectralPars(self, srcName):
+        parameters = self.root.findall('./source[@name="%s"]/spectrum/parameter' % srcName)
+        return parameters
+        
+    def GetSpatialInfo(self, srcName):
         spatialModel = self.root.find('./source[@name="%s"]/spatialModel' % srcName)
         pprint(spatialModel.tag)
         pprint(spatialModel.attrib)
@@ -166,6 +193,10 @@ class LoadModel(NewModel):
         spatialModel = self.root.find('./source[@name="%s"]/spatialModel' % srcName)
         return spatialModel
 
+    def GetSpatialPars(self, srcName):
+        parameters = self.root.findall('./source[@name="%s"]/spatialModel/parameter' % srcName)
+        return parameters
+        
     def GetParInfo(self, srcName, parName):
         parameter = self.root.find('./source[@name="%s"]/spectrum/parameter[@name="%s"]' % (srcName, parName))
         pprint(parameter.tag)
@@ -203,29 +234,26 @@ class LoadModel(NewModel):
     def SetParScaledValue(self, srcName, parName, value):
         parameter = self.root.find('./source[@name="%s"]/spectrum/parameter[@name="%s"]' % (srcName, parName))
         parameter.set('value', str(value))
-        outfile = self.basename + '_SetScaledValue_{}_{}_{}.xml'.format(srcName, parName, value)
-        self.SaveModel(outfile)
+        self.GetModelInfo(self.root)
 
     def SetParScale(self, srcName, parName, scale):
         parameter = self.root.find('./source[@name="%s"]/spectrum/parameter[@name="%s"]' % (srcName, parName))
         parameter.set('scale', str(scale))
-        outfile = self.basename + '_SetParScale_{}_{}_{}.xml'.format(srcName, parName, scale)
-        self.SaveModel(outfile)
+        self.GetModelInfo(self.root)
 
     def SetParFree(self, srcName, parName, free):
         parameter = self.root.find('./source[@name="%s"]/spectrum/parameter[@name="%s"]' % (srcName, parName))
         parameter.set('free', str(free))
-        outfile = self.basename + '_SetFree_{}_{}_{}.xml'.format(srcName, parName, free)
-        self.SaveModel(outfile)
+        self.GetModelInfo(self.root)
 
     def DelSource(self, srcName):
         source = self.root.find('./source[@name="%s"]' % srcName)
         self.root.remove(source)
-        outfile = self.basename + '_DelSource_{}.xml'.format(srcName)
-        self.SaveModel(outfile)
+        self.GetModelInfo(self.root)
 
 
 if __name__ == '__main__':
+    '''
     filename = 'myModel.xml'
     mymodel = NewModel(filename)
 
@@ -240,25 +268,28 @@ if __name__ == '__main__':
     SpatialFile = 'SpatialMap_source.fits'
     SpatialPar = {'name': "Normalization", 'free':1, 'min':0.001, 'max':1000, 'scale':1, 'value':100}
     mymodel.AddDiffuseSource('myDiffuse_source', SpectralType, SpectralPars, SpatialType, SpatialFile, SpatialPar)
-
+    '''
 
     filename = 'XMLmodel.xml'
     model = LoadModel(filename)
-    model.AddPointSource('myPowerLaw_source', SpectralType, SpectralPars, skycrd_C)
+    #model.AddPointSource('myPowerLaw_source', SpectralType, SpectralPars, skycrd_C)
 
     pprint(model.SrcList)
     pprint(model.FixSrcList)
     pprint(model.FreeSrcList)
+    print(model.SrcNum, model.FixSrcNum, model.FreeSrcNum)
+    pprint(model.ParDict)
+    pprint(model.FixParDict)
+    pprint(model.FreeParDict)
     pprint(model.ParList)
     pprint(model.FixParList)
     pprint(model.FreeParList)
-    pprint(model.ParsNumDict)
-    pprint(model.FreeNumDict)
+    print(model.ParNum, model.FixParNum, model.FreeParNum)
 
     srcName = 'PowerLaw_source'
     model.GetSrcInfo(srcName)
-    model.GetSpectralType(srcName)
-    model.GetSpatialType(srcName)
+    model.GetSpectralInfo(srcName)
+    model.GetSpatialInfo(srcName)
 
     parName = 'Prefactor'
     model.GetParInfo(srcName, parName)
@@ -271,4 +302,4 @@ if __name__ == '__main__':
     model.SetParScale(srcName, parName, 1e-10)
     model.SetParFree(srcName, parName, 0)
 
-    model.DelSource('myPowerLaw_source')
+    #model.DelSource('myPowerLaw_source')
